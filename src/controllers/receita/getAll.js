e61dbe968a3e74930acfa40f264d03005d0fe8ab
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-20 12:38:36 -0300

    add route getAll

---
+++ src/controllers/receita/getAll.js
@@ -0,0 +1,19 @@
+import receitaModel from "../../models/receitaModel.js"
+
+const getAll = async(req, res) => {
+    try{
+        const receita = receitaModel.getAll
+        return res.json({
+            success: 'Receitas listadas com sucesso!',
+            receita
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error: 'Ops, erro no servidor.'
+        })
+    }
+}
+
+
+export default getAll
--- src/routers/receitaRouter.js
+++ src/routers/receitaRouter.js
@@ -3,7 +3,7 @@ import criar from '../controllers/receita/criar.js'
 
 const router = express.Router()
 
-// router.get('/', getAll)
+router.get('/', getAll)
 // router.get('/:id', getById)
 // router.put('/:id', update)
 // router.delete('/:id', remove)
