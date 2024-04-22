4eb11d687112544becc34f88dd490f9a4d9dd618
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-22 18:07:53 -0300

    add route edit

--- src/routers/receitaRouter.js
+++ src/routers/receitaRouter.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import criar from '../controllers/receita/criar.js'
 import listar from '../controllers/receita/listar.js'
+import edit from '../controllers/receita/editar.js'
 
 const router = express.Router()
 
 router.get('/', listar)
 // router.get('/:id', getById)
-// router.put('/:id', update)
+router.put('/:id', edit)
 // router.delete('/:id', remove)
 router.post('/', criar)
 
