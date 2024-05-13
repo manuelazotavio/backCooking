465152caca3abb6d0e2bc0cbff8015fd858f774f
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-05-13 17:24:34 -0300

    add login

--- src/config.js
+++ src/config.js
@@ -2,3 +2,9 @@ import 'dotenv/config'
 
 export const PORT = process.env.PORT
 export const HOST = process.env.HOST
+
+export const SECRET_KEY = process.env.SECRET_KEY
+
+
+export const TOKEN_EXPIRES_IN = '3m' // mudar depois para 1h
+export const DB_TOKEN_EXPIRES_DAYS = 1 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+import { SECRET_KEY } from '../config.js'
+
+const auth = (req, res, next) => {
+    //pegar o token
+    let token = false
+    token = req.cookies?.token
+    token = req.headers?.authorization?.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({
+            error: `Usuário não autenticado`
+        })
+    }
+
+    try {
+        const {id , name} = jwt.verify(token, SECRET_KEY)
+        req.userLogged = {id, name, token}
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token expirado.', code: 'expired-token' })
+        }
+        return res.status(401).json({ error: 'Token Inválido.', code: 'invalid-token'})
+    }
+    next()
+}
+
+export default auth
