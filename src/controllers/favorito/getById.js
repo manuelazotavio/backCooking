b045f0b86793752997edc4dd6d06570295af6326
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-05-17 15:56:18 -0300

    add number in gBI

--- src/controllers/favorito/getById.js
+++ src/controllers/favorito/getById.js
@@ -2,7 +2,8 @@ import favModel from "../../models/favModel.js"
 
 const getById = async(req, res) => {
     try{
-        const { userId, receitaId } = req.params;
+        const userId = Number(req.params.userId);
+        const receitaId = Number(req.params.receitaId);
         const favorito = await favModel.getFavorito(userId, receitaId);
         return res.json({
             success: 'Favorito listado com sucesso!',
