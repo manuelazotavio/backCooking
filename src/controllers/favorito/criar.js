a6d8998cd2319596d3b6b663bcc1707f8b2038dc
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-05-19 15:55:30 -0300

    change routes

--- src/controllers/favorito/criar.js
+++ src/controllers/favorito/criar.js
@@ -1,25 +1,23 @@
-import favModel from "../../models/favModel.js"
+import favModel from "../../models/favModel.js";
 
-const criar = async(req, res) => {
-    try{
-        const { receitaId, userId } = req.body
-        const newFavorito = await favModel.create({
-            receitaId: receitaId,
-            userId: userId
-        })
-        console.log(newFavorito)
-        return res.json({
-            success: 'Favoritado com sucesso!',
-            favorito: newFavorito
-        })
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            error: 'Ops, erro no servidor.'
-        })
-    }
-}
+const criar = async (req, res) => {
+  try {
+    const { receitaId, userId } = req.body;
+    const newFavorito = await favModel.create({
+      receitaId,
+      userId,
+    })
+    console.log(newFavorito);
+    return res.json({
+      success: "Favoritado com sucesso!",
+      favorito: newFavorito,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Ops, erro no servidor.",
+    });
+  }
+};
 
-
-export default criar
+export default criar;
