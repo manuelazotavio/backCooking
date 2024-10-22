9be254a4df1ef80b11054743f3c01ffb9e014047
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-22 14:13:20 -0300

    add export default

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,15 +1,18 @@
-import multer from "multer";
+import express from 'express';
+import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+
+const app = express();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads");
     },
     filename: function (req, file, cb) {
-        cb(null, uuidv4() + file.originalname);
+        cb(null, uuidv4() + '-' + file.originalname);
     },
 });
 
 const upload = multer({ storage: storage });
 
-module.exports = upload;
+export default upload
