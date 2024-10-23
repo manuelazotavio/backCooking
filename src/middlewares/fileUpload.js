cb57f7769375101d3dbbe450cb0d8172b3960551
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-22 21:00:48 -0300

    change fileUpload to import

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,16 +1,18 @@
-const multer = require("multer");
-const { v4: uuidv4 } = require('uuid');
+import express from 'express';
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
+const app = express();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads");
     },
     filename: function (req, file, cb) {
-        cb(null, uuidv4() + file.originalname);
+        cb(null, uuidv4() + '-' + file.originalname);
     },
 });
 
 const upload = multer({ storage: storage });
 
-
-export default upload;
+export default upload
