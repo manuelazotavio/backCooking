7db14d33b52d7a78a8a23c24990ec3f9216e45fa
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-23 19:03:22 -0300

    change fileUpload middleware

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
+const app = express();
 
+const uploadDir = path.join(__dirname, 'uploads');
+
+// Verifica se o diretório existe e cria se não existir
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads");
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         cb(null, uuidv4() + '-' + file.originalname);
@@ -15,4 +24,4 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-export default upload
+export default upload;
