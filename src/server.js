83a375d0357a8d9fb096e34cd101388dcb49527e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-23 14:28:00 -0300

    change order

--- src/server.js
+++ src/server.js
@@ -1,39 +1,51 @@
+import express from "express";
+import { PORT, HOST } from "../src/config.js";
+import userRouter from "./routers/userRouter.js";
+import receitaRouter from "./routers/receitaRouter.js";
+import authRouter from "./routers/authRouter.js";
+import logger from "./middlewares/logger.js";
+import favRouter from "./routers/favRouter.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import express from 'express'
-import {PORT, HOST} from '../src/config.js'
-import userRouter from './routers/userRouter.js'
-import receitaRouter from './routers/receitaRouter.js'
-import authRouter from './routers/authRouter.js'
-import logger from './middlewares/logger.js'
-import favRouter from './routers/favRouter.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+const app = express();
 
-const app = express()
-
+app.use(logger);
+app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' https://blog-static.petlove.com.br;");
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' https://blog-static.petlove.com.br;"
+  );
   next();
 });
 
+app.use("/uploads", express.static("uploads"));
 
-app.use('/uploads', express.static('uploads'));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "http://localhost:3001",
+      "http://localhost:8081",
+      "http://localhost:8082",
+    ],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Content-Security-Policy",
+      "Authorization",
+    ],
+  })
+);
 
-app.use(cookieParser())
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3002', 'http://localhost:3001', 'http://localhost:8081', 'http://localhost:8082' ],
-  methods: ['POST', 'GET', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Content-Security-Policy','Authorization']
-}))
-
-app.use(logger)
-app.use(express.json())
-
-app.use('/auth', authRouter)
-app.use('/user', userRouter)
-app.use('/receita', receitaRouter)
-app.use('/favorito', favRouter)
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/receita", receitaRouter);
+app.use("/favorito", favRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${HOST}:${PORT}`)
-})
+  console.log(`Server running on ${HOST}:${PORT}`);
+});
