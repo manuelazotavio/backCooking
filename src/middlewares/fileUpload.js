d6ebc7ca09c6ced4e6ae1ac1c4529c1e35e5b480
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-11-21 18:33:49 -0300

    add req res next

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,23 +1,26 @@
 import multer from "multer";
 
- multer({
+const fileUpload = (req, res, next) =>
+  multer({
     storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, '../uploads/')
-        },
-        filename: (req, file, cb) => {
-            cb(null, Date.now().toString() + "_" + file.originalname)  
-        }
+      destination: (req, file, cb) => {
+        cb(null, "../uploads/");
+      },
+      filename: (req, file, cb) => {
+        cb(null, Date.now().toString() + "_" + file.originalname);
+      },
     }),
     fileFilter: (req, file, cb) => {
-        const extensaoImg = ['image/png', 'image/jpg', 'image/jpeg'].find(formatoAceito => formatoAceito == file.mimetype);
+      const extensaoImg = ["image/png", "image/jpg", "image/jpeg"].find(
+        (formatoAceito) => formatoAceito == file.mimetype
+      );
 
-        if(extensaoImg){
-            return cb(null, true);
-        }
+      if (extensaoImg) {
+        return cb(null, true);
+      }
 
-        return cb(null, false);
-    }
-});
+      return cb(null, false);
+    },
+  });
 
 export default fileUpload;
