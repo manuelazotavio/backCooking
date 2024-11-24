bd9fa7a2a6746fd5fca1fe0572aabae352e9589e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-11-24 16:59:18 -0300

    add bodysparser

--- src/controllers/user/uploadImagem.js
+++ src/controllers/user/uploadImagem.js
@@ -1,11 +1,12 @@
 
 import express from "express";
 import upload from "../../middlewares/fileUpload.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.post("/upload", upload.single('imagem'), async (req, res) => {
 
