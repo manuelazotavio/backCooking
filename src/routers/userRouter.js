b2651e63be12edc717446028cc64f0ae0ef50b7a
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-12-03 16:28:47 -0300

    change imports

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,20 +1,23 @@
-import express from 'express'
-import listAll from '../controllers/user/listAll.js'
-import getById from '../controllers/user/getById.js'
-import create from '../controllers/user/create.js'
-import update from '../controllers/user/update.js'
-import remove from '../controllers/user/remove.js'
+import express from 'express';
+import listAll from '../controllers/user/listAll.js';
+import getById from '../controllers/user/getById.js';
+import create from '../controllers/user/create.js';
+import update from '../controllers/user/update.js';
+import remove from '../controllers/user/remove.js';
+import multer from 'multer';
 import auth from '../middlewares/auth.js';
-import upload from '../middlewares/fileUpload.js';
-import uploadImagem from "../controllers/user/uploadImagem.js"
+import storage from '../middlewares/fileUpload.js';
+import uploadImagem from "../controllers/user/uploadImagem.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', listAll)
-router.get('/:id', getById)
-router.post('/', upload.single('avatar'), create)
-router.post('/upload', uploadImagem)
-router.put('/:id', auth, upload.single('avatar'), update)
-router.delete('/:id', auth, remove)
+const upload = multer({ storage: storage});
 
-export default router
+router.get('/', listAll);
+router.get('/:id', getById);
+router.post('/', upload.single('avatar'), create);
+router.post('/upload', uploadImagem);
+router.put('/:id', auth, upload.single('avatar'), update);
+router.delete('/:id', auth, remove);
+
+export default router;
