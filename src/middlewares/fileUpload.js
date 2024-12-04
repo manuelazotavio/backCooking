73694073dec3365b7f12ce748577bbabc278a1e2
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-12-04 12:58:26 -0300

    change dirname

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,12 +1,21 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    const uploadPath = path.resolve("uploads");
+    const uploadPath = path.resolve(__dirname, "../uploads"); // 
     if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true }); // Cria a pasta se não existir
+      try {
+        fs.mkdirSync(uploadPath, { recursive: true }); // Cria a pasta se não existir
+      } catch (err) {
+        return callback(err);
+      }
     }
     callback(null, uploadPath);
   },
@@ -16,4 +25,8 @@ const storage = multer.diskStorage({
   },
 });
 
-export default storage;
+const upload = multer({
+  storage: storage,
+});
+
+export default upload;
