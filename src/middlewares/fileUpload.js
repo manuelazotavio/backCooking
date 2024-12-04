b75697af2b6c96b650f96143034e8a6462ff2527
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-12-04 13:41:49 -0300

    obrigado a criar a pasta uploads

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -5,13 +5,8 @@ import fs from "fs";
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     const uploadPath = path.resolve("../uploads");
-    if (!fs.existsSync(uploadPath)) {
-      try {
-        fs.mkdirSync(uploadPath, { recursive: true }); // Cria a pasta se não existir
-      } catch (err) {
-        return callback(err);
-      }
-    }
+  
+        fs.mkdirSync(uploadPath, { recursive: true }); // Cria a 
     callback(null, uploadPath);
   },
   filename: (req, file, callback) => {
