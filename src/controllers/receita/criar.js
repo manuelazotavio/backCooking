13c74e59bd966ade6b53a2a74353bf3d5d7fb37d
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-12-05 17:00:56 -0300

    update criar receita reqUrl

--- src/controllers/receita/criar.js
+++ src/controllers/receita/criar.js
@@ -1,47 +1,47 @@
 import zodErrorFormat from "../../helpers/zodErrorFormat.js";
 import receitaModel from "../../models/receitaModel.js";
 
-const criar = async(req, res) => {
-    try {
-        const receita = req.body;
+const criar = async (req, res) => {
+  try {
+    const receita = req.body;
 
-        // Converte userId para inteiro, se existir
-        receita.userId = receita.userId ? parseInt(receita.userId, 10) : null;
-        receita.avaliacao = receita.avaliacao ? parseInt(receita.avaliacao, 10) : null;
+    // Converte userId e avaliacao para inteiros, se existirem
+    receita.userId = receita.userId ? parseInt(receita.userId, 10) : null;
+    receita.avaliacao = receita.avaliacao ? parseInt(receita.avaliacao, 10) : null;
 
-        const foto = req.file ? `/uploads/${req.file.filename}` : null;
+    const foto = req.fileUrl; // URL da imagem enviada pelo middleware
 
-        // Verifica se a imagem foi enviada
-        if (!foto) {
-            return res.status(400).json({ error: "Imagem obrigatória." });
-        }
-        
-        receita.imagem = foto;
-
-        // Validação da receita
-        const result = receitaModel.validateReceitaToCreate(receita);
-        if (!result.success) {
-            return res.status(400).json({
-                error: "Dados inválidos.",
-                fields: zodErrorFormat(result.error)
-            });
-        }
-
-        // Criação da receita no banco
-        const newReceita = await receitaModel.create(result.data);
-
-        // Envia resposta de sucesso, apenas se todos os passos forem concluídos
-        return res.json({
-            success: 'Receita criada com sucesso!',
-            receita: newReceita
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            error: 'Ops, erro no servidor.'
-        });
+    // Verifica se a imagem foi enviada
+    if (!foto) {
+      return res.status(400).json({ error: "Imagem obrigatória." });
     }
-}
+
+    receita.imagem = foto; // Atribui a URL da imagem ao objeto receita
+
+    // Validação da receita
+    const result = receitaModel.validateReceitaToCreate(receita);
+    if (!result.success) {
+      return res.status(400).json({
+        error: "Dados inválidos.",
+        fields: zodErrorFormat(result.error),
+      });
+    }
+
+    // Criação da receita no banco
+    const newReceita = await receitaModel.create(result.data);
+
+    // Resposta de sucesso
+    return res.json({
+      success: 'Receita criada com sucesso!',
+      receita: newReceita,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'Ops, erro no servidor.',
+    });
+  }
+};
 
 export default criar;
