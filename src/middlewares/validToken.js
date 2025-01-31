a0d0668c73ab2532c424df11ef10434677e2c3d9
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-01-31 17:51:44 -0300

    atualizacao de senha consertada

--- src/middlewares/validToken.js
+++ src/middlewares/validToken.js
@@ -1,29 +1,33 @@
 import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "../config.js";
 import userModel from "../models/userModel.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 const validToken = async (req, res) => {
     const { token, newPassword } = req.body;
 
     try {
-      // Verificar o token
-      const decoded = jwt.verify(token, SECRET_KEY);
-      const userId = decoded.id;
-      const user = await userModel.getById(userId)
-   
-      // Atualizar a senha do usuário
-      const hashedPassword = await bcrypt.hash(newPassword, 10); // Hash da senha
-      await userModel.editPass(user, { pass: hashedPassword })
-  
-      res.json({ message: "Senha redefinida com sucesso!" });
+        const decoded = jwt.verify(token, SECRET_KEY);
+        const userId = decoded.id;
+        
+        const user = await userModel.getById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "Usuário não encontrado." });
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        await userModel.editPass(userId, hashedPassword);
+
+        res.json({ message: "Senha redefinida com sucesso!" });
     } catch (error) {
-      console.log(error)
-      if (error.name === "TokenExpiredError") {
-        return res.status(400).json({ error: "Token expirado." });
-      }
-      res.status(400).json({ error: "Token inválido." });
+        console.log(error);
+        if (error.name === "TokenExpiredError") {
+            return res.status(400).json({ error: "Token expirado." });
+        }
+        res.status(400).json({ error: "Token inválido." });
     }
-  };
-  
-  export default validToken;
+};
+
+export default validToken;
+
