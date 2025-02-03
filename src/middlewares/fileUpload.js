4bce4fe401cb21c38b84a66b78973d45ad9aef39
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-02-03 18:47:48 -0300

    fix uploads caracteres

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,34 +1,35 @@
 import supabase from "../helpers/configSupabase.js";
+import { v4 as uuidv4 } from "uuid";
 
 const fileUploadMiddleware = async (req, res, next) => {
-  const file = req.file; 
-  
+  const file = req.file;
+
   if (!file) {
-      return next();
-    }
+    return next();
+  }
 
   try {
-    // Upload para o Supabase
+    const sanitizedFilename = file.originalname
+      .replace(/[^a-zA-Z0-9.]/g, "_")
+      .toLowerCase();
 
-   
+    const uniqueFilename = `${uuidv4()}_${sanitizedFilename}`;
 
     const { data, error } = await supabase.storage
-      .from("images") // Nome do bucket no Supabase
-      .upload(`uploads/${file.originalname}`, file.buffer, {
+      .from("images")
+      .upload(`uploads/${uniqueFilename}`, file.buffer, {
         contentType: file.mimetype,
-        upsert: true, // Substitui arquivos com o mesmo nome
+        upsert: true,
       });
 
     if (error) throw error;
 
-    // Gera URL pública (caso o bucket seja público)
     const { data: publicUrlData } = supabase.storage
       .from("images")
-      .getPublicUrl(`uploads/${file.originalname}`);
-    const publicUrl = publicUrlData.publicUrl;
-
-    req.fileUrl = publicUrl; // Salva a URL para usar depois
-    next(); // Passa o controle para o próximo middleware ou rota
+      .getPublicUrl(`uploads/${uniqueFilename}`);
+      
+    req.fileUrl = publicUrlData.publicUrl;
+    next();
   } catch (err) {
     console.error("Erro no upload:", err.message);
     res.status(500).send("Erro no upload");
